f59f6920d5af0d09de9c2cff0811c9079291830d
Pedrovisk NasciS <phnascis@gmail.com>
2024-11-23 16:22:43 -0300

    Add files via upload

---
+++ Atividades/script.js
@@ -0,0 +1,34 @@
+// Adiciona um evento de clique ao botão de mais informações (primeiro)
+document.getElementById('infoBtn1').addEventListener('click', function() {
+    // Exibe a sobreposição ao clicar no botão
+    document.getElementById('overlay1').style.display = 'flex';
+});
+
+// Adiciona um evento de clique ao botão de fechar (primeiro)
+document.getElementById('closeBtn1').addEventListener('click', function() {
+    // Oculta a sobreposição ao clicar no botão de fechar
+    document.getElementById('overlay1').style.display = 'none';
+});
+
+// Adiciona um evento de clique ao botão de mais informações (segundo)
+document.getElementById('infoBtn2').addEventListener('click', function() {
+    // Exibe a sobreposição ao clicar no botão
+    document.getElementById('overlay2').style.display = 'flex';
+});
+
+// Adiciona um evento de clique ao botão de fechar (segundo)
+document.getElementById('closeBtn2').addEventListener('click', function() {
+    // Oculta a sobreposição ao clicar no botão de fechar
+    document.getElementById('overlay2').style.display = 'none';
+});
+// Adiciona um evento de clique ao botão de mais informações (segundo)
+document.getElementById('infoBtn3').addEventListener('click', function() {
+    // Exibe a sobreposição ao clicar no botão
+    document.getElementById('overlay3').style.display = 'flex';
+});
+
+// Adiciona um evento de clique ao botão de fechar (segundo)
+document.getElementById('closeBtn3').addEventListener('click', function() {
+    // Oculta a sobreposição ao clicar no botão de fechar
+    document.getElementById('overlay3').style.display = 'none';
+});
